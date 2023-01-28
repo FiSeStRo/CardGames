f333ecb6ceffcbd93d1b752356a7de5db66628d8
Finn Steffen <f.ch.steffen.91@gmail.com>
2023-01-28 12:00:11 +0100

    create rest api with GET

--- main.py
+++ main.py
@@ -10,3 +10,27 @@ for i in range(7):
     drawn_card = shuffle_array.pop(0)
     print(texts.get_card_name(drawn_card))
 
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+#MainRoute
+#use for testing values
+@app.route("/", methods=['GET'])
+def index():
+    return "HEllO WORLD"
+
+#Route to get results
+#input-param list of cards
+#return result card list
+@app.route("/result", methods=["GET"])
+def get_cards():
+    input_data = request.json
+    card_list = input_data['cards']
+    # ToDo use CardList to calculate result and return
+    result = {
+        "result": "myResult"
+    }
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run()
